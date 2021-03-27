c31de74639f5370ad1d98ee543a91c738125c01f
Maksym Borysenko <mborysenko@sdl.com>
2021-03-27 09:15:38 +0100

    Refactored color names

--- packages/core/src/appearance/themes/custom/colors.ts
+++ packages/core/src/appearance/themes/custom/colors.ts
@@ -10,24 +10,38 @@ import {
 } from '../../API';
 
 const {
-    POWDER_BLUE, MIDDLE_BLUE, GRAY, LIGHT_GRAY, RED_MUNSELL, CG_BLUE, INDIGO_DYE, MANTIS, BRIGHT_YELLOW_CRYOLA, BABY_POWDER, PRUSSIAN_BLUE, TEA_GREEN, GUN_METAL
+    POWDER_BLUE,
+    MIDDLE_BLUE,
+    GRAY,
+    LIGHT_GRAY,
+    RED_MUNSELL,
+    CG_BLUE,
+    INDIGO_DYE,
+    MANTIS,
+    BRIGHT_YELLOW_CRYOLA,
+    BABY_POWDER,
+    PRUSSIAN_BLUE,
+    TEA_GREEN,
+    GUN_METAL,
+    FOGRA_29
 } = PALETTE;
 
 export const colors: ColorScheme = {
     [ColorScope.APPLICATION]: {
-        [ColorScopeApplication.BG_REGULAR]: LIGHT_GRAY,
+        [ColorScopeApplication.BG]: LIGHT_GRAY,
+        [ColorScopeApplication.FG]: FOGRA_29,
     },
     [ColorScope.TYPOGRAPHY]: {
         [ColorScopeTypography.TEXT_REGULAR]: GRAY,
-        [ColorScopeTypography.TEXT_CONTRAST]: BABY_POWDER,
+        [ColorScopeTypography.TEXT_DARK]: BABY_POWDER,
         [ColorScopeTypography.LINK]: POWDER_BLUE,
         [ColorScopeTypography.LINK_HOVER]: PRUSSIAN_BLUE,
         [ColorScopeTypography.HEADING]: PRUSSIAN_BLUE,
     },
     [ColorScope.CONTAINER]: {
         [ColorScopeContainer.BORDER]: TEA_GREEN,
-        [ColorScopeContainer.FILL_REGULAR]: GUN_METAL,
-        [ColorScopeContainer.FILL_CONTRAST]: INDIGO_DYE,
+        [ColorScopeContainer.BG]: GUN_METAL,
+        [ColorScopeContainer.FG]: INDIGO_DYE,
     },
     [ColorScope.ACTIONS]: {
         [ColorScopeActions.ACTION]: MANTIS,
@@ -35,9 +49,9 @@ export const colors: ColorScheme = {
         [ColorScopeActions.COMMON]: MIDDLE_BLUE,
     },
     [ColorScope.MESSAGES]: {
-        [ColorScopeMessages.WARNING]: BRIGHT_YELLOW_CRYOLA,
-        [ColorScopeMessages.INFO]: CG_BLUE,
-        [ColorScopeMessages.ERROR]: RED_MUNSELL,
-        [ColorScopeMessages.SUCCESS]: MANTIS,
+        [ColorScopeMessages.WARNING_BG]: BRIGHT_YELLOW_CRYOLA,
+        [ColorScopeMessages.INFO_BG]: CG_BLUE,
+        [ColorScopeMessages.ERROR_BG]: RED_MUNSELL,
+        [ColorScopeMessages.SUCCESS_BG]: MANTIS,
     },
 }
--- packages/core/src/routing/dom/Area/API.ts
+++
@@ -1,9 +0,0 @@
-enum LayoutAreas {
-    TOP = 'top',
-    CENTER = 'center',
-    LEFT = 'left',
-    RIGHT = 'right',
-    BOTTOM = 'bottom',
-}
-
-export { LayoutAreas };
