1a885c553fe624e5e05409726c5e9692bb53c372
Maksym Borysenko <mborysenko@sdl.com>
2021-04-17 21:41:48 +0200

    * Added check if the route we try to extend exists. * Added check if the route we try to insert exists.

--- packages/core/src/routing/utils/extendRoute.ts
+++ packages/core/src/routing/utils/extendRoute.ts
@@ -9,10 +9,14 @@ export const extendRoute = (parts: string[], route: RouteWithChildren) => {
     if (parentRoute) {
         const routes = parentRoute?.routes || [];
 
+        if(routes.some(r => r.path === route.path)) {
+            throw new Error(`Route with path ${route.path} already exists, please choose correct one.`)
+        }
+
         const newRoute = assoc('routes', [...routes!, route], parentRoute);
 
         updateRoute(parts, newRoute);
     } else {
-        throw new Error(`No such route ${JSON.stringify(parts)}`)
+        throw new Error(`No such route ${JSON.stringify(parts)}.`)
     }
 }
