244c822f50209289b8267f8454b068cd37920a0d
Maksym Borysenko <maksym.borysenko@trendyol.com>
2023-12-03 21:49:56 +0100

    - Removes CircleCI integration - Fixes `className` type error

--- packages/core/src/components/layout/Box/API.ts
+++ packages/core/src/components/layout/Box/API.ts
@@ -1,5 +1,7 @@
-export interface BoxProps {
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+
+export type BoxProps = {
     greedy?: boolean;
     holdsAbsolute?: boolean;
     alignItems?: string;
-}
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
